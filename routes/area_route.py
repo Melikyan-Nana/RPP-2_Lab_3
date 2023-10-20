128a0d783438e530582499b8534114a72c177e89
Melikyan-Nana <114040581+Melikyan-Nana@users.noreply.github.com>
2023-10-20 21:35:58 +0700

    Create area_route.py

---
+++ routes/area_route.py
@@ -0,0 +1,59 @@
+from dict import tax_dict
+from flask import Blueprint, request, jsonify, abort
+
+bp = Blueprint('area_route', __name__)
+
+
+@bp.route("/v1/add/tax", methods=['POST'])
+def add_tax():
+    data = request.get_json()
+    code = data['code']
+    tax = data['tax']
+    if code in tax_dict:
+        abort(400)
+    else:
+        tax_dict[code] = tax
+        message = {'message': 'Tax with code added'}
+        return jsonify(message), 201
+
+
+@bp.route("/v1/update/tax", methods=['POST'])
+def update_tax():
+    data = request.get_json()
+    code = data['code']
+    tax = data['tax']
+    if code in tax_dict:
+        tax_dict[code] = tax
+        message = {'message': f'Tax with code updated'}
+        return jsonify(message), 201
+    else:
+        abort(400)
+
+
+@bp.route("/v1/fetch/taxes", methods=['GET'])
+def fetch_taxes():
+    return jsonify(tax_dict), 200
+
+
+@bp.route('/v1/fetch/tax', methods=['GET'])
+def fetch_tax():
+    code = request.args.get('code')
+    if code in tax_dict:
+        message = {"Tax": tax_dict[code]}
+        return jsonify(message), 200
+    else:
+        abort(400)
+
+
+@bp.route('/v1/fetch/calc', methods=['GET'])
+def fetch_calc():
+    code = request.args.get('code')
+    cadastral_value = int(request.args.get('cadastral_value'))
+    month_of_ownership = int(request.args.get('month_of_ownership'))
+    if code in tax_dict:
+        tax = int(tax_dict[code])
+        tax_for_year = (cadastral_value * tax * month_of_ownership)/12
+        message = {"Tax amount for the year": tax_for_year}
+        return jsonify(message), 200
+    else:
+        abort(400)
